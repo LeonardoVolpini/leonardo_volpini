35cf6ce14d93db6842f4684d023247f0a193398c
LeonardoVolpini <volpini.leonardo00@gmail.com>
2025-03-22 15:12:10 +0100

    Levate Skills dalla Home e al loro posto inseriti i Servizi. Aggiunto vercel analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,14 @@ import Head from "next/head";
 // pages/index.tsx
 import { Header } from '@/components/Header'
 import { AboutMe } from '@/components/About'
-import { Skills } from '@/components/Skills'
 //import { Projects } from '@/components/Projects'
 import { Contacts } from '@/components/Contacts'
 import { HeroSection } from '@/components/Hero'
 import { Footer } from '@/components/Footer'
+import Services from "@/components/Services";
 
 //import { projects } from "@/utils/consts"
-import { skills } from '@/utils/consts'
-import { navItems } from '@/utils/consts'
+import { services, navItems } from '@/utils/consts'
 import { CookieBanner } from '@/components/CookieBanner'
 
 export default function Home() {
@@ -26,11 +25,9 @@ export default function Home() {
       <main className="min-h-screen">
         <HeroSection />
         <AboutMe />
-        <Skills items={skills} />
         {/*<Projects items={projects}/>  {/*TODO: quando avrò i progetti da mostrare*/}
-        <h2 className="text-3xl font-bold text-center mt-12">
-          Contattami
-        </h2>
+        <Services items={services}/>
+        <br />
         <Contacts />
         <Footer />
         <CookieBanner />
